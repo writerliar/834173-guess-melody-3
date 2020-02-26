ad7879671f2e00ae48a55556a4ec1ce1e7cd28c7
Dana <kovach.dana@yandex.ru>
2020-02-26 12:15:02 +0300

    добавила ActionCreator в reducer

--- src/reducer.js
+++ src/reducer.js
@@ -10,6 +10,13 @@ const ActionType = {
   INCREMENT_STEP: `INCREMENT_STEP`,
 };
 
+const ActionCreator = {
+  incrementStep: () => ({
+    type: ActionType.INCREMENT_STEP,
+    payload: 1,
+  })
+};
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.INCREMENT_STEP:
@@ -26,4 +33,4 @@ const reducer = (state = initialState, action) => {
   return state;
 };
 
-export {reducer, ActionType};
+export {reducer, ActionType, ActionCreator};
